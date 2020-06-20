dd1d8532b0d96e083ae2369cd03d493ea1cdf82f
am0tta <amotta3@miners.utep.edu>
2020-06-20 06:49:46 -0600

    Adding helpers.c file

---
+++ src/helpers.c
@@ -0,0 +1,36 @@
+#include "malloc.h"
+#include "tonekizer.h"
+
+//true if c is a tab or space, and not zero
+int space_char(char c){
+
+
+
+
+}
+
+//true if c is not a tab or space, and not zero
+int non_space_char(char c){
+}
+
+// * to first (non-space) char in first word in s
+char *word_start(char *s){
+
+}
+
+// * to char after end of word
+char *word_terminator(char *word){
+}
+
+// the number of words in s
+int count_words(char *s){
+}
+
+char *copy_str(char *inStr, short len){
+}
+
+void print_tokens(char**){
+}
+
+void free_tokens(char**){
+}
