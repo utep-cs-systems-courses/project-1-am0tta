17bd30ec9f3005e7b9ac7ad6994c91ccd361df32
Alan Motta <amotta3@miners.utep.edu>
2020-06-23 00:41:36 -0600

    Defined space_char and non_space_char

--- src/helpers.c
+++ src/helpers.c
@@ -1,20 +1,30 @@
 #include "malloc.h"
-#include "tonekizer.h"
+#include "tokenizer.h"
 
 //true if c is a tab or space, and not zero
 int space_char(char c){
 
+  if (c == '\t' || c == ' ')
+    return 1;
 
-
+  else
+    return 0;
 
 }
 
 //true if c is not a tab or space, and not zero
 int non_space_char(char c){
+
+  if (c >= 34 && c <= 126)
+    return 1;
+
+  else
+    return 0;
+  
 }
 
 // * to first (non-space) char in first word in s
-char *word_start(char *s){
+char word_start(char *str){
 
 }
 
@@ -23,14 +33,14 @@ char *word_terminator(char *word){
 }
 
 // the number of words in s
-int count_words(char *s){
+int count_words(char *str){
 }
 
 char *copy_str(char *inStr, short len){
 }
 
-void print_tokens(char**){
+void print_tokens(char **tokens){
 }
 
-void free_tokens(char**){
+void free_tokens(char **tokens){
 }
