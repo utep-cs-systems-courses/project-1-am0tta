612b3d37768f52933c2697c9fc4b15695545e880
Alan Motta <amotta3@miners.utep.edu>
2020-06-25 03:31:19 -0600

    Testing *word_start and *word_terminator

--- src/main.c
+++ src/main.c
@@ -13,16 +13,25 @@ int main(){
   printf("Enter your sentence. When you are ready to quit simply type 0.\n");
   printf("< ");
   fgets(user_input, MAX, stdin);
+
   
   //exit() function to end the program
   if(user_input[0] == '0'){
   printf("\n");
-  printf("Goodbye\n");
+  printf("Goodbye :)\n");
     exit(0);
   }
   
   printf("%s\n", user_input);
 
+  //testing word_start
+  char *p = word_start(user_input);
+  printf("Word starts at: %s\n", p);
+
+  //testing word_terminator
+  char *po = word_terminator(user_input);
+  printf("Word ends at: %s\n", po);
+  
   
   //The program keeps going until the user types 0
   if(user_input[0]!= '0'){
