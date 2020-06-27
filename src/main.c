8e93d8fa4e52815642e5d0fb830174afdbad685e
Alan Motta <amotta3@miners.utep.edu>
2020-06-27 03:04:33 -0600

    Edited indentation

--- src/main.c
+++ src/main.c
@@ -7,35 +7,41 @@
 void initiate_input(char c);
 int main(){
   
-  //Building a simple UI with fgets() - Milestone 1
+  // Building a simple UI with fgets()
   char user_input[100];
+  printf("\n");
   printf("Welcome.\n");
   printf("Enter your sentence. When you are ready to quit simply type 0.\n");
   printf("< ");
   fgets(user_input, MAX, stdin);
 
   
-  //exit() function to end the program
+  // exit() function to end the program
   if(user_input[0] == '0'){
-  printf("\n");
-  printf("Goodbye :)\n");
+    
+    printf("\n");
+    printf("Goodbye :)\n");
     exit(0);
+    
   }
   
   printf("%s\n", user_input);
 
-  //testing word_start
+  // testing word_start
   char *p = word_start(user_input);
   printf("Word starts at: %s\n", p);
 
-  //testing word_terminator
+  // testing word_terminator
   char *po = word_terminator(user_input);
   printf("Word ends at: %s\n", po);
   
+  // testing count_words
+  int words = count_words(user_input);
+  printf("The number of words is: %d\n", words); 
   
-  //The program keeps going until the user types 0
-  if(user_input[0]!= '0'){
+  // The program keeps going until the user types 0
+  if(user_input[0]!= '0')
     int keepUp = main();
-  }
+  
   return 0;
 }
