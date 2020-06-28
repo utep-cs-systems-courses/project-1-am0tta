9591c09916dae5ed36a4265070999dd3303007af
Alan Motta <amotta3@miners.utep.edu>
2020-06-28 04:09:38 -0600

    Defined tokenize function

--- src/helpers.c
+++ src/helpers.c
@@ -5,25 +5,25 @@
 int space_char(char c){
 
   if(c == '\t' || c == ' ')
-    return 1;
+    return 1;                 // true, since C does not handle boolean variables
 
   else
-    return 0;
+    return 0;                // false
 
 }
 
 // True if c is not a tab or space, and not zero
 int non_space_char(char c){
 
-  if(c != '\t' && c != ' ')
-    return 1;
+  if(c != '\t' && c != ' ')    
+    return 1;                // 1 stands for true in C
 
   else
-    return 0;
+    return 0;               // 0 stands for false in C
   
 }
 
-// * to first (non-space) char in first word in str
+// pointer to first (non-space) char in first word in str
 char *word_start(char *str){
   
   while(*str){           //while something is here
@@ -37,7 +37,7 @@ char *word_start(char *str){
 
 }
 
-// * to char after end of word
+// pointer to char after end of word
 char *word_terminator(char *word){
 
   while(*word){           //while something is here
@@ -77,7 +77,7 @@ int count_words(char *str){
 // Allocates some space for the words it copies
 char *copy_str(char *inStr, short len){
 
-  char * pointer = malloc(len + 1); //must allocate extra space for zero('\0') terminator
+  char * pointer = malloc(len + 1); //must allocate extra space for zero ('\0') terminator
   
   int i;
   for(i = 0; i <= len; i++)
@@ -90,6 +90,7 @@ char *copy_str(char *inStr, short len){
 
 // All tokens will be printed
 void print_tokens(char **tokens){
+  
   int i = 0;
   while(*tokens){
 
@@ -110,5 +111,31 @@ void free_tokens(char **tokens){
 
   }
   free(tokens);
+  
+}
+
+/* Returns a freshly allocated vector of freshly allocated
+   space-separated tokens from zero terminated str. */
+char **tokenize(char *str){
+
+  
+  char *original = str;                 // pointer to keep an eye on the provided string
+  int words = count_words(string);
+  char **tokens = malloc (sizeof(char *) *(words + 1));
+  char *final = word_terminator(str);         //pointer at the end
+
+  for(int i = 0; i < words; i++){
+
+    if(i > 0)
+      original = words_start(words);
+    
+    x = word_terminator(str) - str;
+    toekns = copi_st(str,x);
+
+    str = word_terminator(str);
+
+  }
+  tokens[i] = '\0';
+  return tokens;
   
 }
